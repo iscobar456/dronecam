a96d875a5dd5abc17380b34b180e876a07fd2515
isaac <isaacspencer17@gmail.com>
2026-05-16 19:05:40 -0600

    removed turn

--- client/src/ConnectionManager.cpp
+++ client/src/ConnectionManager.cpp
@@ -129,15 +129,14 @@ void ConnectionManager::closeConnection() {
 
 void ConnectionManager::createRtcPC() {
   rtcConfiguration config{};
-  const char *iceServers[] = {"stun:stun.relay.metered.ca:80",
-                              "turn:bfc22cc224cb894f60cff28a:"
-                              "KkdyHKXNNh8ptCyj@standard.relay.metered.ca:"
-                              "80",
-                              "turn:bfc22cc224cb894f60cff28a:"
-                              "KkdyHKXNNh8ptCyj@standard.relay.metered.ca:"
-                              "443"};
+  const char *iceServers[] = {"stun:stun.relay.metered.ca:80"};
+  // "turn:bfc22cc224cb894f60cff28a:"
+  // "KkdyHKXNNh8ptCyj@standard.relay.metered.ca:"
+  // "80",
+  // "turn:bfc22cc224cb894f60cff28a:"
+  // "KkdyHKXNNh8ptCyj@standard.relay.metered.ca:"
+  // "443"};
 
-  //                            "stun:stun.actionvoip.com:3478"};
   config.iceServers = iceServers;
   config.iceServersCount = 2;
   config.bindAddress = "0.0.0.0";
